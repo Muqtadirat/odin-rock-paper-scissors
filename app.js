cf3d45354903ba9da42718d12f37070af2611867
Muqtadirat <muqtadir.my@gmail.com>
2023-12-31 12:24:24 +0100

    feat: score display

--- app.js
+++ app.js
@@ -1,13 +1,21 @@
 "use strict";
 
+var playerChoice;
+// let playerChoice = prompt("Choose wisely", "").toUpperCase();
+let playerScore = 0;
+let computerScore = 0;
+let roundsLeft = 5;
+
+// if (playerChoice === "" || playerChoice === null) {
+//   alert("Invalid input");
+// } else playerChoice;
+
 function getComputerChoice() {
   let rock = "ROCK";
   let scissors = "SCISSORS";
   let paper = "PAPER";
 
   let choices = parseInt(Math.random() * 4);
-
-  console.log(choices);
 
   if (choices == 1) {
     return rock;
@@ -18,4 +26,51 @@ function getComputerChoice() {
   }
 }
 
-console.log(getComputerChoice());
+function playRound(playerSelection, computerSelection) {
+  const playerScoreBox = document.querySelector(".player-score");
+  const computerScoreBox = document.querySelector(".computer-score");
+
+  playerChoice = prompt("Choose wisely", "").toUpperCase();
+
+  if (playerChoice === "" || playerChoice === null) {
+    alert("Invalid input");
+  } else playerChoice;
+
+  if (
+    (playerSelection === "ROCK" && computerSelection === "SCISSORS") ||
+    (playerSelection === "PAPER" && computerSelection === "ROCK") ||
+    (playerSelection === "SCISSORS" && computerSelection === "PAPER")
+  ) {
+    playerScore++;
+    playerScoreBox.textContent = `${playerScore}`;
+  } else if (
+    (playerSelection === "ROCK" && computerSelection === "PAPER") ||
+    (playerSelection === "PAPER" && computerSelection === "SCISSORS") ||
+    (playerSelection === "SCISSORS" && computerSelection === "ROCK")
+  ) {
+    computerScore++;
+    computerScoreBox.textContent = `${computerScore}`;
+  } else if (playerSelection === computerSelection) {
+    alert("It's tie, replay");
+  } else alert("Not a wise choice");
+
+  return;
+}
+
+function game() {
+  const roundsDisplay = document.querySelector(".rounds-left");
+  const playerSelection = playerChoice;
+  const computerSelection = getComputerChoice();
+
+  for (let round = 0; round < 5; round++) {
+    playRound(playerSelection, computerSelection);
+
+    roundsLeft--;
+    roundsDisplay.textContent = `${roundsLeft}`;
+  }
+}
+
+// console.log(getComputerChoice());
+// console.log(playerChoice);
+
+game();
